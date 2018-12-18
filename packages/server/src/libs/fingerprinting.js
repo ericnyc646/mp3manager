3828867b5ac858ae2ab4b2d320e150a94818a30b
Christian Castelli <voodoo81people@gmail.com>
2018-12-18 19:35:05 +0100

    package-lock updated

--- packages/server/src/libs/fingerprinting.js
+++ packages/server/src/libs/fingerprinting.js
@@ -1,5 +1,11 @@
 import fpcalc from 'fpcalc';
 
+/**
+ * It calculates an AcousticID fingerprint. Using different versions of the library, 
+ * produces a different hash.
+ * @param {String|Stream} obj it must be the path to an audio file or a readable stream.
+If using a stream, note that you will not get duration out due to an fpcalc issue
+ */
 export function getAcusticId(obj) {
     return new Promise((resolve, reject) => {
         fpcalc(obj, (err, result) => {
@@ -10,4 +16,10 @@ export function getAcusticId(obj) {
             return resolve(result);
         });
     });
+}
+
+export class AcusticId {
+    constructor(key) {
+        this.key = key;
+    }
 }
