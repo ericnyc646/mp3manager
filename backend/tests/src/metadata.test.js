206305e01b95b161d8d82a1be06688c1a4160980
Borewit <borewit@users.noreply.github.com>
2019-10-15 21:09:45 +0200

    Use music-metadata to extract metadata

--- backend/tests/src/metadata.test.js
+++ backend/tests/src/metadata.test.js
@@ -1,32 +1,30 @@
-const fs = require('fs');
-const MetaInfo = require('../../src/libs/scanner/mediainfo');
-const EyeD3 = require('../../src/libs/eyeD3');
+const mm = require('music-metadata');
+const path = require('path');
 
 describe('ID3 Tags', () => {
-    const resDir = path.join(__dirname, '..', 'resources');
-    const resFile = path.join(resDir, 'Under The Ice (Scene edit).mp3');
+  const resDir = path.join(__dirname, '..', 'resources');
+  const resFile = path.join(resDir, 'Under The Ice (Scene edit).mp3');
 
-    it('Mediainfo', async () => {
-        const metadata = await MetaInfo.getData(resFile);
+  it('Mediainfo', async () => {
+    const metadata = await mm.parseFile(resFile);
 
-        expect(metadata).not.toBeNull();
-        expect(metadata.size).toBe(5235428);
-        expect(metadata.duration).toBe(128.888);
-        expect(metadata.bitrate.mode).toBe('CBR');
-        expect(metadata.bitrate.value).toBe(320);
-        expect(metadata.title).toBe('Under The Ice (Scene edit)');
-        expect(metadata.album).toBe('Lives Of The Artists: Follow Me Down - Soundtrack');
-        expect(metadata.album_performer).toBe('UNKLE');
-        expect(metadata.performer).toBe('UNKLE');
-        expect(metadata.recorded_date.toISOString().split('T')[0]).toBe('2010-01-01');
-        expect(metadata.last_modified.toISOString()).toEqual('2019-03-25T11:25:56.000Z');
-        expect(metadata.encoded_library).toBe('LAME3.99r');
-        expect(metadata.image.mime).toBe('image/jpeg');
-    });
+    expect(metadata).not.toBeNull();
+    // expect(metadata.size).toBe(5235428);
+    expect(metadata.format.duration).toBeCloseTo(128.888);
+    expect(metadata.format.container).toBe('MPEG');
+    expect(metadata.format.codec).toBe('MP3');
+    expect(metadata.format.codecProfile).toBe('CBR');
+    expect(metadata.format.bitrate).toBe(320000);
+    expect(metadata.common.title).toBe('Under The Ice (Scene edit)');
+    expect(metadata.common.album).toBe('Lives Of The Artists: Follow Me Down - Soundtrack');
+    expect(metadata.common.artist).toBe('UNKLE');
+    expect(metadata.common.date).toBe('2010');
+    expect(metadata.format.tool).toBe('LAME3.99r');
+  });
 
-    it('eyeD3 image extraction', async () => {
-        const imgPath = await EyeD3.getCoverImage(resFile);
-        expect(fs.existsSync(imgPath)).toBeTruthy();
-        fs.unlinkSync(imgPath);
-    });
+  it('eyeD3 image extraction', async () => {
+    const metadata = await mm.parseFile(resFile);
+    expect(metadata.common.picture).toBeDefined();
+    expect(metadata.common.picture[0].format).toBe('image/jpeg');
+  });
 });
